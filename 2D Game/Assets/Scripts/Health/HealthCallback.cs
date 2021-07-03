973895212e70992382224458a67f427a24e94a04
drago314 <avrickaltmann@yahoo.com>
2021-07-02 19:50:34 -0700

    Add Health, Damage, and Spikes

---
+++ 2D Game/Assets/Scripts/Health/HealthCallback.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHealthCallback
+{
+    void OnHit();
+    void OnDeath();
+    void OnHeal();
+    void OnHealthChanged(int currentHealth, int MaxHealth);
+}
